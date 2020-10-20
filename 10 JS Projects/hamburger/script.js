2026cd69db5a519744653b60c9ea3c46acc84699
Arash <arashonquora@gmail.com>
2020-10-21 01:07:13 +0530

    updates

--- 10 JS Projects/hamburger/script.js
+++ 10 JS Projects/hamburger/script.js
@@ -1,3 +1,10 @@
 // basic script file setup
 
 // write JavaScript Only!
+const btn = document.getElementById("btn");
+const nav = document.getElementById("nav");
+
+btn.addEventListener("click", () => {
+    nav.classList.toggle("active");
+    btn.classList.toggle("active");
+});
